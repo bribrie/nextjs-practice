aa96be3585d491b2775833600fb96520adb91be2
bribrie <ibriebriee@gmail.com>
2022-05-28 23:22:25 +0900

    Implement Sending request using api routes

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,24 @@
 import NewMeetupForm from "@/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
-const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+function NewMeetupPage() {
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
-};
+}
 
 export default NewMeetupPage;
